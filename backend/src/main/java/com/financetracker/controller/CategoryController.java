3fe38c348585daa51bf9299f5d9ce8ce846d0c71
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-06-17 22:06:45 +0530

    Added category services and controller

--- backend/src/main/java/com/financetracker/controller/CategoryController.java
+++ backend/src/main/java/com/financetracker/controller/CategoryController.java
@@ -1,14 +1,15 @@
 package com.financetracker.controller;
 
 import com.financetracker.dto.request.CategoryRequest;
-import com.financetracker.dto.response.ApiResponse;
 import com.financetracker.dto.response.CategoryResponse;
 import com.financetracker.enums.TransactionType;
 import com.financetracker.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 /**
@@ -16,12 +17,11 @@ import java.util.List;
  *
  * Base URL: /api/v1/categories
  *
- * TODO (Milestone 2):
- *  POST   /         → createCategory()           → 201
- *  GET    /         → getAllCategories()          → 200
- *  GET    /?type=   → getCategoriesByType()       → 200
- *  PUT    /{id}     → updateCategory()            → 200
- *  DELETE /{id}     → deleteCategory()            → 204
+ * POST   /             → createCategory()      → 201
+ * GET    /             → getAllCategories()    → 200
+ * GET    /type?type=   → getCategoriesByType() → 200
+ * PUT    /{id}         → updateCategory()      → 200
+ * DELETE /{id}         → deleteCategory()      → 204
  */
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -30,6 +30,48 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    // TODO: Implement endpoints
+    @PostMapping
+    public ResponseEntity<CategoryResponse> createCategory(
+            @Valid @RequestBody CategoryRequest request) {
 
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(categoryService.createCategory(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+
+        return ResponseEntity.ok(
+                categoryService.getAllCategories()
+        );
+    }
+
+    @GetMapping("/type")
+    public ResponseEntity<List<CategoryResponse>> getCategoriesByType(
+            @RequestParam TransactionType type) {
+
+        return ResponseEntity.ok(
+                categoryService.getCategoriesByType(type)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(
+            @PathVariable Long id,
+            @Valid @RequestBody CategoryRequest request) {
+
+        return ResponseEntity.ok(
+                categoryService.updateCategory(id, request)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(
+            @PathVariable Long id) {
+
+        categoryService.deleteCategory(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/financetracker/service/impl/CategoryServiceImpl.java
+++ backend/src/main/java/com/financetracker/service/impl/CategoryServiceImpl.java
@@ -2,12 +2,21 @@ package com.financetracker.service.impl;
 
 import com.financetracker.dto.request.CategoryRequest;
 import com.financetracker.dto.response.CategoryResponse;
+import com.financetracker.entity.Category;
+import com.financetracker.entity.User;
 import com.financetracker.enums.TransactionType;
+import com.financetracker.exception.DuplicateResourceException;
 import com.financetracker.repository.CategoryRepository;
+import com.financetracker.repository.UserRepository;
 import com.financetracker.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.print.attribute.UnmodifiableSetException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,31 +34,146 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
 
     @Override
     public CategoryResponse createCategory(CategoryRequest request) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow();
+
+        if(categoryRepository.existsByNameAndUserId(
+                request.getName(),
+                user.getId()
+        )){
+            throw new DuplicateResourceException(
+                    "Category already exists"
+            );
+        }
+
+        Category category = Category.builder()
+                .name(request.getName())
+                .icon(request.getIcon())
+                .color(request.getColor())
+                .type(request.getType())
+                .isDefault(false)
+                .user(user)
+                .build();
+
+        categoryRepository.save(category);
+
+
+        return CategoryResponse.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .icon(category.getIcon())
+                .color(category.getColor())
+                .type(category.getType())
+                .isDefault(category.isDefault())
+                .build();
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<CategoryResponse> getAllCategories() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow();
+
+        List<Category> categories = user.getCategories();
+
+        List<Category> userCategories = categoryRepository.findAllByUserId(user.getId());
+        List<Category> defaultCategories = categoryRepository.findAllByIsDefaultTrue();
+        userCategories.addAll(defaultCategories);
+
+        List<CategoryResponse> responses = new ArrayList<>();
+        for(Category category : userCategories){
+
+            responses.add(
+                    CategoryResponse.builder()
+                            .id(category.getId())
+                            .name(category.getName())
+                            .icon(category.getIcon())
+                            .color(category.getColor())
+                            .type(category.getType())
+                            .isDefault(category.isDefault())
+                            .build()
+            );
+
+        }
+
+        return responses;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<CategoryResponse> getCategoriesByType(TransactionType type) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow();
+
+        List<Category> categories =
+                categoryRepository.findAllByUserIdAndType(
+                        user.getId(),
+                        type
+                );
+
+        List<CategoryResponse> responses = new ArrayList<>();
+
+        for(Category category : categories){
+            responses.add(CategoryResponse.builder()
+                    .id(category.getId())
+                    .name(category.getName())
+                    .icon(category.getIcon())
+                    .color(category.getColor())
+                    .type(category.getType())
+                    .isDefault(category.isDefault())
+                    .transactionCount((long)category.getTransactions().size())
+                    .build()
+            );
+        }
+
+        return responses;
     }
 
     @Override
     public CategoryResponse updateCategory(Long id, CategoryRequest request) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow();
+        Category category = categoryRepository.findById(id).orElseThrow();
+
+        category.setName(request.getName());
+        category.setIcon(request.getIcon());
+        category.setColor(request.getColor());
+        category.setType(request.getType());
+
+        Category updatedCategory =
+                categoryRepository.save(category);
+
+        return CategoryResponse.builder()
+                .id(updatedCategory.getId())
+                .name(updatedCategory.getName())
+                .icon(updatedCategory.getIcon())
+                .color(updatedCategory.getColor())
+                .type(updatedCategory.getType())
+                .isDefault(updatedCategory.isDefault())
+                .transactionCount(
+                        (long) updatedCategory.getTransactions().size()
+                )
+                .build();
     }
 
     @Override
     public void deleteCategory(Long id) {
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Category category = categoryRepository.findById(id).orElseThrow();
+
+        if (!category.getTransactions().isEmpty()) {
+            throw new IllegalStateException
+                    ("Category is in use by " + category.getTransactions().size() + " transactions");
+        }
+
+        categoryRepository.delete(category);
     }
 }
